c72764074ce2bce692bc35fa310f80fede0abc1f
julia <mombachdasilva@gmail.com>
2025-10-29 15:30:20 -0300

    trabalhos

--- build/tp3/CMakeFiles/tp3_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tp3_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tp3_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
